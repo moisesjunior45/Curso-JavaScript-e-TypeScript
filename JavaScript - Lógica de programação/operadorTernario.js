192ade647f37ec8cdaa430e45c9d8e9a987b834c
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-04-19 16:12:11 -0300

    Estudos dia 19/04

---
+++ JavaScript - Lógica de programação/operadorTernario.js
@@ -0,0 +1,10 @@
+// (condição) ? 'Valor para verdadeiro' : 'Valor para falso';
+const pontuacaoUsuario = 999;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? "Usuário Vip": "Usuário Padrão";
+console.log(nivelUsuario);
+
+/* if (pontuacaoUsuario >= 1000) {
+    console.log("Usuário vip")
+} else {
+    console.log("Usuário Padrão")
+} */
