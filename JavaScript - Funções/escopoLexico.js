038ed8128140d053c58aef66da00b5b02500bb28
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-06-07 16:13:00 -0300

    Estudos 07/06

---
+++ JavaScript - Funções/escopoLexico.js
@@ -0,0 +1,12 @@
+const nome = 'Moisés';
+
+function falaNome() {
+    console.log(nome);
+}
+falaNome();
+
+function usaFalaNome() {
+    const nome = 'Otávio'; 
+    falaNome();
+}
+usaFalaNome();
--- JavaScript - Funções/retornoFuncao.js
+++ JavaScript - Funções/retornoFuncao.js
@@ -1,5 +1,38 @@
 // return - Retorna um valor, Termina a função
-function soma(a, b) {
-    return a + b;
+function criaPessoa(nome, sobrenome) {
+    return {nome, sobrenome };
 }
-console.log(soma(2,5))
+
+const p1 = criaPessoa('Moisés', 'Oliveira');
+const p2 = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+}; 
+
+console.log(p1);
+console.log(p2);
+
+// function duplica(n) {
+//     return n * 2;
+// }
+
+// function triplica(n) {
+//     return n * 3;
+// }
+
+// function quadriplica(n) {
+//     return n * 4;
+// }
+function criaMultiplicador(multiplicador) {
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(3));
+console.log(triplica(2));
+console.log(quadriplica(10));
