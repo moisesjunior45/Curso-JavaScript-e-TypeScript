c7578e66962fb89390f889f11711eb9980627da6
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-07-19 08:23:29 -0300

    Update filtrandoOArray.js

--- JavaScript - Arrays/filtrandoOArray.js
+++ JavaScript - Arrays/filtrandoOArray.js
@@ -1,9 +1,30 @@
-//  Filter, map, reduce 
+//  Filter -> Sempre vai retornar um array, com a mesma quantidade de elementos ou menos.
 
 // Retorne os números maiores que 10
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const numerosFiltrados = numeros.filter((valor, indice, array) => {
-    console.log(valor, indice, array);
-    return valor > 10;
-});   
-console.log(numerosFiltrados);
+const numerosFiltrados = numeros.filter(valor => valor > 10);   
+// console.log(numerosFiltrados);
+
+
+
+// Retorne as pessoas que tem o nome com 5 letras ou mais.
+// Retorne as pessoas com mais de 50 anos.
+// Retorne as pessoas cujo nome termina com "a"
+
+const pessoas = [
+    { nome: 'Luiz', idade: 62},
+    { nome: 'Maria', idade: 23},
+    { nome: 'Eduardo', idade: 55},
+    { nome: 'Letícia', idade: 19},
+    { nome: 'Rosana', idade: 32},
+    { nome: 'Wallace', idade: 47},
+];
+const pessoasComNomeGrande = pessoas.filter(obj => obj.nome.length >= 5);
+const pessoasComMaisDeCinquenta = pessoas.filter(obj => obj.idade > 50);
+const NomesTerminadosEmA = pessoas.filter(obj => {
+    return obj.nome.toLowerCase().endsWith('a');    //indica se uma string termina com caracteres determinados, retornando  true ou  false.
+});
+
+console.log(pessoasComNomeGrande);
+console.log(pessoasComMaisDeCinquenta);
+console.log(NomesTerminadosEmA);
