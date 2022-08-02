01bb15c8824a7fd1274c70ed327675e91043f838
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-08-02 11:42:09 -0300

    Update heranca.js

--- JavaScript - Objetos e Prototypes/heranca.js
+++ JavaScript - Objetos e Prototypes/heranca.js
@@ -14,9 +14,15 @@ Produto.prototype.desconto = function (quantia) {
 
 function Camiseta(nome, preco, cor) {
     Produto.call(this, nome, preco);
+    this.cor = cor;
 }
 Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+Camiseta.prototype.aumento = function (percentual) {
+    this.preco = this.preco + (this.preco * (percentual / 100));
+}
 
+const produto = new Produto('Gen', 111);
 const camiseta = new Camiseta('Regata', 7.5, 'Preta');
-camiseta.aumento(10);
+console.log(produto);
 console.log(camiseta);
