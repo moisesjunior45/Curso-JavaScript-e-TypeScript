8578425e20c9aaf265e5bb3792640a3e3ba9a285
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-09-02 15:38:56 -0300

    Adicionando arquivos

---
+++ JavaScript Classes - POO (Programação Orientada a Objetos)/metodosInstanciaeEstaticos.js
@@ -0,0 +1,26 @@
+class ControleRemoto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+    }
+    // Método de instância
+    aumentarVolume() {
+        this.volume += 2;
+    }
+    // Método de instância
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+
+    // Método estático
+    static trocaPilha() {
+        console.log('Ok, vou trocar.');
+    }
+}
+
+const controle1 = new ControleRemoto('LG');
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
+ControleRemoto.trocaPilha();
