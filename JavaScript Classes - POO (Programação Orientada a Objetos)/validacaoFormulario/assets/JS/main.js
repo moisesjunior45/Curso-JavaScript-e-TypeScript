bab78ffac7c68d952337745fdf9216bcfa6b389f
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-09-09 15:50:22 -0300

    Editando arquivos

--- JavaScript Classes - POO (Programação Orientada a Objetos)/validacaoFormulario/assets/JS/main.js
+++ JavaScript Classes - POO (Programação Orientada a Objetos)/validacaoFormulario/assets/JS/main.js
@@ -1,28 +1,56 @@
 class ValidaFormulario {
-    constructor() {
-        this.formulario = document.querySelector('.formulario');
-        this.eventos();
+  constructor() {
+    this.formulario = document.querySelector('.formulario');
+    this.eventos();
+  }
+
+  eventos() {
+    this.formulario.addEventListener('submit', e => {
+      this.handleSubmit(e);
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const camposValidos = this.camposSaoValidos();
+  }
+  camposSaoValidos() {
+    let valid = true;
+
+    for (let errorText of this.formulario.querySelectorAll('.error-text')) {
+      errorText.remove();
+    }
+
+    for (let campo of this.formulario.querySelectorAll('.validar')) {
+      const label = campo.previousElementSibling.innerText;
+
+
+      if (!campo.value) {
+        this.criaErro(campo, `Campo "${label}" não pode estar em branco. `);
+        valid = false;
       }
 
-      eventos() {
-        this.formulario.addEventListener('submit', e => {
-          this.handleSubmit(e);
-        });
+      if(campo.classList.contains('cpf')) {
+        if(!this.validaCPF(campo)) valid = false;
       }
-      handleSubmit(e) {
-        e.preventDefault();
-        const camposValidos = this.camposSaoValidos();
     }
-    camposSaoValidos() {
-        let valid = true;
+  }
 
-        for(let campo of this.formulario.querySelectorAll('.validar')) {
-            console.log(campo)
-        }
+  validaCPF(campo) {
+    const cpf = new ValidaCPF(campo.value);
+
+    if(!cpf.valida()) {
+      this.criaErro(campo, 'CPF inválido.');
+      return false;
     }
+    return true;
+  }
+
+  criaErro(campo, msg) {
+    const div = document.createElement('div');
+    div.innerHTML = msg;
+    div.classList.add('error-text');
+    campo.insertAdjacentElement('afterend', div);
+  }
 }
-
-
- 
 
 const valida = new ValidaFormulario();
