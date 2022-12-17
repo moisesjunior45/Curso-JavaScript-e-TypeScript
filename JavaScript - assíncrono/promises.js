43074f7f2e6ac483c0d6bb5156e407218f0995f9
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2022-12-16 23:40:24 -0300

    Update promises.js

--- JavaScript - assíncrono/promises.js
+++ JavaScript - assíncrono/promises.js
@@ -6,15 +6,31 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
+        if (typeof msg !== 'string') reject(new Error('Erro'));
         setTimeout(() => {
             resolve(msg);
         }, tempo);
     });
 }
 
-esperaAi('Frase 1', rand(1, 3))
-.then(resposta => {
-    console.log(resposta);
-})
-.catch();
+esperaAi('Conexão com o BD', rand(1, 3))
+    .then(resposta => {
+        console.log(resposta);
+        return esperaAi('Buscando dados da BASE', rand(1, 3));
+    })
+    .then(resposta => {
+        console.log(resposta);
+        return esperaAi(22222, rand(1, 3));
+    })
+    .then(resposta => {
+        console.log(resposta);
+    })
+    .then(() => {
+        console.log('Exibe dados na tela');
+    })
+    .catch(e => {
+        console.log('ERRO:', e);
+    });
+
+console.log('Isso aqui será exibido antes de qualquer promisse.');
 
