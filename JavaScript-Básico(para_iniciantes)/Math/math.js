68f83ec364c7f88991268a7c9c78a032e1aa9a0c
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-08-10 21:47:22 -0300

    Finalizando projeto Gerador de senha

--- JavaScript-Básico(para_iniciantes)/Math/math.js
+++ JavaScript-Básico(para_iniciantes)/Math/math.js
@@ -3,7 +3,7 @@ let num1 = 9.54678;
 // let num2 = Math.ceil(num1);      //Arredonda o número para cima
 // let num2 = Math.round(num1);     //Arredonda o número para o mais próximo
 // console.log(Math.max(1,2,3,4,5,-10,-50,1500,9));    //Retorna o maior número da sequência
-// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9));        //Retorna o maior número da sequência
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9));        //Retorna o menor número da sequência
 // console.log(Math.random())      //Retorna um número aleátorio entre 0 e 1 (o 1 não é incluido)
 // const numeroAleaotorio = Math.round(Math.random() * 11);
 // console.log(Math.PI)     //Retorna PI
---
+++ JavaScript-Tooling e ES6 Modules/Gerador_Senhas/src/modules/geradores.js
@@ -0,0 +1,22 @@
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const geraMaiscula = () => String.fromCharCode(rand(65, 91));
+const geraMinuscula = () => String.fromCharCode(rand(97, 123));
+const geraNumeros = () => String.fromCharCode(rand(48, 58));
+const simbolos = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+const geraSimbolo = () => simbolos[rand(0, simbolos.length)];
+
+export default function geraSenha(qtd, maisculas, minusculas, numeros, simbolos) {
+    const senhaArray = [];
+    qtd = Number(qtd)
+
+    for (let i = 0; i < qtd; i++) {
+        maisculas && senhaArray.push(geraMaiscula());
+        minusculas && senhaArray.push(geraMinuscula());
+        numeros && senhaArray.push(geraNumeros());
+        simbolos && senhaArray.push(geraSimbolo());
+    }
+
+     senhaArray.sort(() => 0.5 - Math.random());
+    return senhaArray.join('').slice(0, qtd);
+}
+
