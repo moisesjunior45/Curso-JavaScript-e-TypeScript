feb1a6ed6246a34223f3117f2b213902048a7d3e
Moisés Júnior <84156546+moisesjunior45@users.noreply.github.com>
2024-10-31 22:31:23 -0300

    Express e nodemon

---
+++ node/Aula07-Express+Nodemon/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`
+    <form action="/" method="POST">
+        Nome do cliente: <input type="text name="nome">
+        <button>Enviar</button>
+    </form>
+        `);
+});
+
+app.post("/", (req, res) => {
+    res.send('Recebi o formulário');
+})
+
+app.get("/contato", (req, res) => {
+    res.send("Obrigado por entrar em contato com a gente");
+});
+
+app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+});
